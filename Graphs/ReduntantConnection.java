a3aba199e520bb704a02dbcd1d2f88554ebbccaf
deepakcse1 <deepak.kumar.ext1@holcim.com>
2026-06-20 22:14:09 +0530

    added DFS code for Reduntant Connection

--- Graphs/ReduntantConnection.java
+++ Graphs/ReduntantConnection.java
@@ -5,7 +5,6 @@ public class ReduntantConnection {
 
   int[] parent;
   int[] rank;
-
   public int[] findRedundantConnection(int[][] edges) {
     int n = edges.length;
     parent = new int[n + 1];
@@ -38,4 +37,33 @@ public class ReduntantConnection {
     if (parent[el] == el) return el;
     return parent[el] = find(parent[el]);
   }
+    //-------------------------------DFS Solution------------------
+    // public int[] findRedundantConnection(int[][] edges) {
+    //     int n = edges.length;
+    //     List<Integer>[] list = new ArrayList[n+1];
+    //     for(int i = 0; i <= n; i++){
+    //         list[i] = new ArrayList<>();
+    //     }
+    //     for(int[] edge : edges){
+    //         int[] visited = new int[n+1];
+    //         int u = edge[0];
+    //         int v = edge[1];
+    //         if(dfs(u, v, visited, list)){
+    //             return edge;
+    //         }
+    //         list[u].add(v);
+    //         list[v].add(u);
+    //     }
+    //     return new int[]{-1,-1};
+    // }
+    // public boolean dfs(int src, int dest, int[] visited, List<Integer>[] list){
+    //     if(src == dest) return true;
+    //     visited[src] = 1;
+    //     for(int neigh : list[src]){
+    //         if(visited[neigh] == 0){
+    //             if(dfs(neigh, dest, visited, list)) return true;
+    //         }
+    //     } 
+    //     return false;
+    // }
 }
