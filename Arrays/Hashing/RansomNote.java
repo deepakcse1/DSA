ffd1e565c87251b90ac0634cef7a716e691f8ca4
deepakcse1 <deepak.kumar.ext1@holcim.com>
2026-07-18 14:29:20 +0530

    Lt - 283, Ransom Note

---
+++ Arrays/Hashing/RansomNote.java
@@ -0,0 +1,18 @@
+package Hashing;
+
+public class RansomNote {
+  public boolean canConstruct(String ransomNote, String magazine) {
+    if (ransomNote.length() > magazine.length()) return false;
+    int[] freq = new int[26];
+    for (char c : ransomNote.toCharArray()) {
+      freq[c - 'a']++;
+    }
+    for (char c : magazine.toCharArray()) {
+      freq[c - 'a']--;
+    }
+    for (int i = 0; i < 26; i++) {
+      if (freq[i] > 0) return false;
+    }
+    return true;
+  }
+}
