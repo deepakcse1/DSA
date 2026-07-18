3318b22dd8b5f13d7942890e89c284f7f7c84035
deepakcse1 <deepak.kumar.ext1@holcim.com>
2026-07-18 14:29:00 +0530

    Lt - 274, H Index

---
+++ Arrays/H_Index.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class H_Index {
+  public int hIndex(int[] citations) {
+    Arrays.sort(citations);
+    int n = citations.length;
+    for (int i = 0; i < n; i++) {
+      int papers = n - i;
+      if (citations[i] >= papers) return papers;
+    }
+    return 0;
+  }
+}
