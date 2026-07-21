7c7d88ca2655625d8d0ca57163acb47613f68f84
deepakcse1 <deepak.kumar.ext1@holcim.com>
2026-07-21 22:34:49 +0530

    Lt - 18, Four Sum (formatted)

--- Arrays/FourSum.java
+++ Arrays/FourSum.java
@@ -64,31 +64,40 @@ public class FourSum {
   }
 
   //two pointer still O(n*3)
-  public List<List<Integer>> fourSumOptimized(int[] nums, int target) {
+
+  public List<List<Integer>> fourSum_optimized(int[] nums, int target) {
+    List<List<Integer>> result = new ArrayList<>();
     Arrays.sort(nums);
     int n = nums.length;
-    List<List<Integer>> res = new ArrayList<>();
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n - 3; i++) {
       if (i > 0 && nums[i] == nums[i - 1]) continue;
-      for (int j = i + 1; j < n; j++) {
+      if ((long) nums[i] + nums[i + 1] + nums[i + 2] + nums[i + 3] > target) break;
+      if ((long) nums[i] + nums[n - 1] + nums[n - 2] + nums[n - 3] < target) continue;
+      for (int j = i + 1; j < n - 2; j++) {
         if (j > i + 1 && nums[j] == nums[j - 1]) continue;
+        if ((long) nums[i] + nums[j] + nums[j + 1] + nums[j + 2] > target) break;
+        if ((long) nums[i] + nums[j] + nums[n - 1] + nums[n - 2] < target) continue;
         int k = j + 1;
         int l = n - 1;
         while (k < l) {
-          long sum = nums[i]; 
-          sum += nums[j];
-          sum += nums[k];
-          sum += nums[l];
+          long sum = (long) nums[i] + nums[j] + nums[k] + nums[l];
           if (sum == target) {
-            res.add(Arrays.asList(nums[i], nums[j], nums[k], nums[l]));
-            k++; l--;
+            result.add(Arrays.asList(nums[i], nums[j], nums[k], nums[l]));
+            k++;
+            l--;
             while (k < l && nums[k] == nums[k - 1]) k++;
             while (k < l && nums[l] == nums[l + 1]) l--;
-          } else if (sum < target) k++;
-          else l--;
+          } else if (sum > target) { 
+            l--;
+            while (k < l && nums[l] == nums[l + 1]) l--;
+          } else { 
+            k++;
+            while (k < l && nums[k] == nums[k - 1]) k++;
+          }
         }
       }
     }
-    return res;
+    return result;
   }
 }
+
