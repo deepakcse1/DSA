1f85c0c1cbcf48d8260260bb8bc1526cf83398be
deepakcse1 <deepak.kumar.ext1@holcim.com>
2026-07-21 22:35:16 +0530

    Lt - 290, Word Pattern(commited)

--- Arrays/Hashing/WordPattern.java
+++ Arrays/Hashing/WordPattern.java
@@ -1,26 +1,27 @@
 package Hashing;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 public class WordPattern {
-  public boolean wordPattern(String pattern, String s) {
-        Map<Character, String> pToW = new HashMap<>();
-        Map<String, Character> wToP = new HashMap<>();
+    public boolean wordPattern(String pattern, String s) {
+        int n = pattern.length();
         String[] words = s.split(" ");
-        if(pattern.length() != words.length) return false;
-        for(int i = 0; i < pattern.length(); i++){
-            char ch = pattern.charAt(i);
+        if (n != words.length) return false;
+        Map<Character, String> map = new HashMap<>();
+        Set<String> set = new HashSet<>();
+        for (int i = 0; i < n; i++) {
+            char key = pattern.charAt(i);
             String word = words[i];
-            if(pToW.containsKey(ch)){
-                if(!pToW.get(ch).equals(word)) return false;
+            if (map.containsKey(key)) {
+                if (!map.get(key).equals(word)) return false;
+            } else {
+                if (set.contains(word)) return false;
+                set.add(word);
+                map.put(key, words[i]);
             }
-            pToW.put(ch, word);
-
-            if(wToP.containsKey(word)){
-                if(wToP.get(word) != ch) return false;
-            }
-            wToP.put(word, ch);
         }
         return true;
     }
