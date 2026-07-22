560be16f218016473ac1fdcad1d43a9141f80f3e
deepakcse1 <deepak.kumar.ext1@holcim.com>
2026-07-22 20:18:20 +0530

    Lt - 454, 4Sum 2

---
+++ Arrays/Hashing/Four_Sum2.java
@@ -0,0 +1,22 @@
+package Hashing;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Four_Sum2 {
+  public int fourSumCount(int[] nums1, int[] nums2, int[] nums3, int[] nums4) {
+    Map<Integer, Integer> map = new HashMap<>();
+    for (int i : nums1) {
+      for (int j : nums2) {
+        map.put(i + j, map.getOrDefault(i + j, 0) + 1);
+      }
+    }
+    int count = 0;
+    for (int i : nums3) {
+      for (int j : nums4) {
+        count += map.getOrDefault(-(i + j), 0);
+      }
+    }
+    return count;
+  }
+}
