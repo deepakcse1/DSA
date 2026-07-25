1badc8bf2f37ac6c69ac0979b5aeeb1b1f094a84
deepakcse1 <deepak.kumar.ext1@holcim.com>
2026-07-25 08:59:09 +0530

    Formated Code

---
+++ Arrays/BinarySearch/SearchInsertPosition.java
@@ -0,0 +1,16 @@
+package BinarySearch;
+
+public class SearchInsertPosition {
+  public int searchInsert(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length-1;
+        while(start <= end){
+            int mid = start + (end - start)/2;
+            int cur = nums[mid];
+            if(cur == target) return mid;
+            else if(cur < target) start = mid + 1;
+            else end = mid-1;
+        }
+        return start;
+    }
+}
