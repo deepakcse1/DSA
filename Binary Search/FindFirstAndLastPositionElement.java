66fa197c8de4ae140e883c42edb96bbf11e611e3
deepakcse1 <deepak.kumar.ext1@holcim.com>
2026-07-26 15:18:16 +0530

    LtC - 34, Find First and Last Position of Element

---
+++ Binary Search/FindFirstAndLastPositionElement.java
@@ -0,0 +1,39 @@
+public class FindFirstAndLastPositionElement {
+  public int[] searchRange(int[] nums, int target) {
+    int left = binarySearch(nums, target, true);
+    int right = binarySearch(nums, target, false);
+    return new int[] { left, right };
+  }
+
+  private int binarySearch(int[] nums, int target, boolean isLeft) {
+    int index = -1;
+    int start = 0;
+    int end = nums.length - 1;
+    while (start <= end) {
+      int mid = start + (end - start) / 2;
+      if (nums[mid] == target) {
+        index = mid;
+        if (isLeft) end = mid - 1;
+        else start = mid + 1;
+      } else if (nums[mid] < target)
+        start = mid + 1;
+      else
+        end = mid - 1;
+    }
+    return index;
+  }
+    // private int searcInRight(int[] nums, int target){
+    //     int index = -1;
+    //     int start = 0;
+    //     int end = nums.length-1;
+    //     while(start <= end){
+    //         int mid = start + (end - start)/2;
+    //         if(nums[mid] == target){
+    //             index = mid;
+    //             start = mid + 1;
+    //         }else if(nums[mid] < target) start = mid + 1;
+    //         else end = mid - 1;
+    //     }
+    //     return index;
+    // }
+}
