20025974377b4eaaa29a55d4e0eb6c2c62562096
deepakcse1 <deepak.kumar.ext1@holcim.com>
2026-07-26 15:19:57 +0530

    LtC - 28, Find Index of First Occurrence

---
+++ String/FindTheIndexOfFirstOccurence.java
@@ -0,0 +1,72 @@
+package String;
+
+public class FindTheIndexOfFirstOccurence {
+  private int[] buildLPS(char[] pattern) {
+    int len = 0;
+    int i = 1;
+    int n = pattern.length;
+    int[] lps = new int[n];
+    while (i < n) {
+      if (pattern[len] == pattern[i]) {
+        len++;
+        // lps[i] stores the length of the longest proper prefix which is also a suffix for the substring patter [0...i]
+        lps[i] = len; 
+        i++;
+      } else {
+        if (len == 0)
+          i++;
+        else {
+          len = lps[len - 1];
+        }
+      }
+    }
+    return lps;
+  }
+
+  public int strStr(String haystack, String needle) {
+    // KMP - Knuth-Morris-Pratt algo
+    char[] pattern = needle.toCharArray();
+    // 1. build LPS
+    int[] lps = buildLPS(pattern);
+    // 2. Perform KMP search using LPS array
+    int i = 0; // haystack
+    int j = 0; // needle
+    int m = haystack.length();
+    int n = needle.length();
+    while (i < m) {
+      if (haystack.charAt(i) == needle.charAt(j)) {
+        i++;
+        j++;
+        if (j == n)
+          return i - n; // whole pattern is matched
+      } else {
+        if (j == 0)
+          i++;
+        else
+          j = lps[j - 1];
+      }
+    }
+    return -1;
+  }
+
+    //Brute Force O(n*m)
+    // public int strStr(String haystack, String needle) {
+    //     int m = haystack.length();
+    //     int n = needle.length();
+    //     if(n > m) return -1;
+    //     for(int i = 0; i <= m-n; i++){
+    //         // Without extra space------------------------------------
+    //         int j = 0;
+    //         while(j < n && haystack.charAt(i+j) == needle.charAt(j)){
+    //             j++;
+    //         }
+    //         if(j == n) return i;
+
+    //         //Extra space taken-----------------------------------------
+    //         // if(haystack.charAt(i) == needle.charAt(0)){
+    //         //     if(i+n <= m && haystack.substring(i, i+n).equals(needle)) return i;
+    //         // }
+    //     }
+    //     return -1;
+    // }
+}
