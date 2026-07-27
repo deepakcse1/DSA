8fec07414355f032a81c8c65e15667b3dce8556c
deepakcse1 <deepak.kumar.ext1@holcim.com>
2026-07-27 11:24:45 +0530

    H Index Modified

--- Arrays/H_Index.java
+++ Arrays/H_Index.java
@@ -1,6 +1,23 @@
 import java.util.Arrays;
 
 public class H_Index {
+  //----------O(n)----------------
+  public int hIndex_optimized(int[] citations) {
+    int n = citations.length;
+    int[] count = new int[n + 1];
+    for (int i : citations) {
+      if (i >= n) count[n]++;
+      else count[i]++;
+    }
+    int paper = 0;
+    for (int h = n; h >= 0; h--) {
+      paper += count[h];
+      if (paper >= h) return h;
+    }
+    return 0;
+  }
+
+  //----------O(nLogn)----------------
   public int hIndex(int[] citations) {
     Arrays.sort(citations);
     int n = citations.length;
