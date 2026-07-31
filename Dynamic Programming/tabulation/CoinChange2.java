c0edd6fa528f38544c228785cd2488b7306ce830
deepakcse1 <deepak.kumar.ext1@holcim.com>
2026-07-31 23:14:21 +0530

    LtC - 518, Coin Change II

--- Dynamic Programming/tabulation/CoinChange2.java
+++ Dynamic Programming/tabulation/CoinChange2.java
@@ -5,19 +5,50 @@ public class CoinChange2 {
 
   }
 
+  public int change_opt(int amount, int[] coins) {
+    int[] dp = new int[amount + 1];
+    dp[0] = 1;
+    for (int coin : coins) {
+      for (int am = coin; am <= amount; am++) {
+        dp[am] += dp[am - coin];
+      }
+    }
+    return dp[amount];
+  }
+
+  public int change_optimized(int amount, int[] coins) {
+    int n = coins.length;
+    int[] next = new int[amount + 1];
+    next[0] = 1;
+    for (int idx = n - 1; idx >= 0; idx--) {
+      int[] curr = new int[amount + 1];
+      curr[0] = 1;
+      for (int am = 1; am <= amount; am++) {
+        int take = 0;
+        if (am >= coins[idx]) {
+          take = curr[am - coins[idx]];
+        }
+        int notTake = next[am];
+        curr[am] = take + notTake;
+      }
+      next = curr;
+    }
+    return next[amount];
+  }
+
   public int change(int amount, int[] coins) {
     int[][] dp = new int[coins.length + 1][amount + 1];
     for (int i = 0; i <= coins.length; i++) {
       dp[i][0] = 1;
     }
-    for (int index = coins.length - 1; index >= 0; index--) {
-      for (int target = 1; target <= amount; target++) {
+    for (int idx = coins.length - 1; idx >= 0; idx--) {
+      for (int am = 1; am <= amount; am++) {
         int take = 0;
-        if (target - coins[index] >= 0) {
-          take = dp[index][target - coins[index]];
+        if (am - coins[idx] >= 0) {
+          take = dp[idx][am - coins[idx]];
         }
-        int notTake = dp[index + 1][target];
-        dp[index][target] = take + notTake;
+        int notTake = dp[idx + 1][am];
+        dp[idx][am] = take + notTake;
       }
     }
     return dp[0][amount];
