1873def2082a1cc7b9b8a8d789da342bd6ffa973
deepakcse1 <deepak.kumar.ext1@holcim.com>
2026-07-31 23:12:19 +0530

    LtC-380, Insert Delete GetRandom O(1)

--- Arrays/InsertDeleteGetRandom.java
+++ Arrays/InsertDeleteGetRandom.java
@@ -11,37 +11,34 @@ class RandomizedSet {
     Map<Integer, Integer> map;
     List<Integer> list;
     public RandomizedSet() {
-        map = new HashMap<>();
-        list = new ArrayList<>();
+      map = new HashMap<>();
+      list = new ArrayList<>();
     }
     
     public boolean insert(int val) {
-        if(map.containsKey(val)) return false;
-        list.add(val);
-        map.put(val, list.size()-1);
-        return true;
+      if(map.containsKey(val)) return false;
+      list.add(val);
+      map.put(val, list.size()-1);
+      return true;
     }
     
     public boolean remove(int val) {
-        if(!map.containsKey(val)) return false;
-        int index = map.get(val);
-        if(index == list.size()-1){
-            list.remove(list.size()-1);
-            map.remove(val);
-            return true;
-        }
+      if(!map.containsKey(val)) return false;
+      int index = map.get(val);
+      if(index != list.size()-1){
         int last = list.get(list.size()-1);
         list.set(index, last);
-        list.remove(list.size()-1);
         map.put(last, index);
-        map.remove(val);
-        return true;
+      }
+      list.remove(list.size()-1);
+      map.remove(val);
+      return true;
     }
     
     public int getRandom() {
-        // return list.get((int) (Math.random() * list.size()));
-        Random random = new Random();
-        return list.get(random.nextInt(list.size()));
+      // return list.get((int) (Math.random() * list.size()));
+      Random random = new Random();
+      return list.get(random.nextInt(list.size()));
     }
 }
 
