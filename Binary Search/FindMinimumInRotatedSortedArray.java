322f8a98dccd9457f810a0bd185384fb8cd2057b
deepakcse1 <deepak.kumar.ext1@holcim.com>
2026-07-31 23:12:38 +0530

    LtC - 153, Find Minimum in Rotated Sorted Array

--- Binary Search/FindMinimumInRotatedSortedArray.java
+++ Binary Search/FindMinimumInRotatedSortedArray.java
@@ -2,14 +2,11 @@ public class FindMinimumInRotatedSortedArray {
   public int findMin(int[] nums) {
     int start = 0;
     int end = nums.length - 1;
-    int resultIndex = 0;
-    while (start <= end) {
+    while (start < end) {
       int mid = start + (end - start) / 2;
-      if (nums[mid] < nums[resultIndex]) resultIndex = mid;
-      if (nums[mid] <= nums[end]) { end = mid - 1;
-      } else { start = mid + 1;
-      }
+      if (nums[mid] > nums[end]) start = mid + 1;
+      else end = mid;
     }
-    return nums[resultIndex];
+    return nums[start];
   }
 }
